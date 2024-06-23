c5ac0cea2bbcc92a0b1bfb2c34eca90e2aac1a15
pobertt <79865756+pobertt@users.noreply.github.com>
2024-06-23 14:01:20 +0100

    added assets, weapon animations and input for shooting
    4 different weapons; knife, gun, smg and minigun
    a small issue to think about- rate of fire/ammo is subtracted based on tick rate (smg for example, ammo = 10 but only shoots 7 bullets because of the speed/frame rate)

---
+++ UI/ui.gd
@@ -0,0 +1,45 @@
+extends CanvasLayer
+
+var time_since_last_shot : float = 0.0
+var fire_rate : float = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.animation_finished.connect(_on_AnimatedSprite2D_animation_finished)
+	$AnimatedSprite2D.play(Global.current_weapon + "_idle")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time_since_last_shot += delta
+	var can_shoot : bool = time_since_last_shot >= (1.0 / fire_rate)
+	
+	if Global.current_weapon != "knife" and Global.ammo <= 0:
+		Global.current_weapon = "knife"
+		$AnimatedSprite2D.play("knife_idle")
+	
+	if Input.is_action_pressed("ui_select") and can_shoot:
+		if Global.current_weapon == "knife":
+			$AnimatedSprite2D.play("knife_stab")
+		else:
+			$AnimatedSprite2D.play(Global.current_weapon + "_shoot")
+			
+		time_since_last_shot = 0
+		
+		if Global.current_weapon != "knife":
+			if Global.ammo > 0:
+				Global.ammo -= 1
+		
+	match Global.current_weapon:
+		"gun":
+			fire_rate = 3.0
+		"smg":
+			fire_rate = 6.0
+		"minigun":
+			fire_rate = 10.0
+		"knife":
+			fire_rate = 2.0
+		_:
+			fire_rate = 1.0
+			
+func _on_AnimatedSprite2D_animation_finished():
+	$AnimatedSprite2D.play(Global.current_weapon + "_idle")
---
+++ global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var ammo = 10
+var current_weapon = "smg"
